f83105845ffb0da4dfe4a587a685935075b8b9d8
reynaldo <reynaldoqs@gmail.com>
2019-11-25 16:25:46 -0400

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudApp.Models;
+using CrudApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UsersRP RepoUsers = new UsersRP();
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return RepoUsers.getUsers();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            return RepoUsers.getUser(id);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public IActionResult Post(User value)
+        {
+            RepoUsers.addUser(value);
+            return CreatedAtAction(nameof(Post), value);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User value)
+        {
+            RepoUsers.updateUser(id, value);
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            RepoUsers.removeUser(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/UsersRP.cs
@@ -0,0 +1,44 @@
+﻿using CrudApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Repositories
+{
+    public class UsersRP
+    {
+        public static List<User> _users = new List<User>(){
+               new User(){Id = 1, Name = "Maria", Credit = 23.0},
+               new User(){Id = 2, Name = "Juana", Credit = 623.0},
+               new User(){Id = 3, Name = "Carlos", Credit = 491.0},
+               new User(){Id = 4, Name = "Pedro", Credit = 123.0},
+               new User(){Id = 5, Name = "Gomer", Credit = 53.0},
+               new User(){Id = 6, Name = "Moralex", Credit = 33.0},
+        };
+
+        public IEnumerable<User> getUsers()
+        {
+            return _users;
+        }
+        public User getUser(int id)
+        {
+            var client = _users.Where(x => x.Id == id);
+            return client.FirstOrDefault();
+        }
+        public void addUser(User newUser)
+        {
+            _users.Add(newUser);
+        }
+        public void updateUser(int id, User user)
+        {
+            int index = _users.FindIndex(x => x.Id == id);
+            _users[index] = user;
+        }
+        public void removeUser(int id)
+        {
+            int index = _users.FindIndex(x => x.Id == id);
+            _users.RemoveAt(index);
+        }
+    }
+}
